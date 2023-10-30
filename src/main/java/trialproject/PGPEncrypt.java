2e67cd83006f07616801c1e0b2cb18debdfa4e8b
Sayantan DS <dassharma.sayantan@gmail.com>
2023-10-30 17:45:49 +0530

    draft commit

---
+++ src/main/java/trialproject/PGPEncrypt.java
@@ -0,0 +1,208 @@
+package trialproject;
+
+import org.bouncycastle.bcpg.ArmoredOutputStream;
+import org.bouncycastle.openpgp.*;
+import org.bouncycastle.openpgp.operator.jcajce.JcePublicKeyKeyEncryptionMethodGenerator;
+import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator;
+import org.bouncycastle.openpgp.operator.jcajce.JcePGPDataEncryptorBuilder;
+import java.io.*;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.util.Iterator;
+
+
+public class PGPEncrypt 
+{
+	public static void encryptMyFile(
+			String inputFilePath,
+			String outputFilePath,
+			PGPPublicKey[] recipientPublicKeys,
+			boolean armor
+			) throws Exception {
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+		//FileOutputStream out = new FileOutputStream(outputFilePath);
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFilePath));
+		if(armor)
+		{
+			out = new ArmoredOutputStream(out);        
+		}
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+		PGPCompressedDataGenerator comData = new PGPCompressedDataGenerator(
+				PGPCompressedData.ZIP);
+
+		PGPUtil.writeFileToLiteralData(
+				comData.open(bOut),
+				PGPLiteralData.BINARY,
+				new File(inputFilePath));
+
+		comData.close();
+
+		PGPEncryptedDataGenerator cPk = new PGPEncryptedDataGenerator(
+				new JcePGPDataEncryptorBuilder(
+						PGPEncryptedData.CAST5)
+				.setWithIntegrityPacket(true)
+				.setSecureRandom(
+						new SecureRandom())
+				.setProvider("BC"));
+
+		for (PGPPublicKey publicKey : recipientPublicKeys) {
+			cPk.addMethod(
+					new JcePublicKeyKeyEncryptionMethodGenerator(publicKey)
+					.setProvider("BC"));
+		}
+
+		byte[] bytes = bOut.toByteArray();
+
+		OutputStream cOut = cPk.open(out, bytes.length);
+
+		cOut.write(bytes);
+
+		cOut.close();
+
+		out.close();
+	}
+	public static void encryptFile(
+			String inputFilePath,
+			String outputFilePath,
+			String compositePublicKeyPath,
+			boolean armor
+			) throws Exception {
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+		PGPPublicKeyRingCollection pgpPublicKeyRingCollection = new PGPPublicKeyRingCollection(
+				PGPUtil.getDecoderStream(new FileInputStream(compositePublicKeyPath)), new JcaKeyFingerprintCalculator());        
+
+		System.out.println( "Loaded Pub Key" );
+		Iterator<PGPPublicKeyRing> keyRingIterator = pgpPublicKeyRingCollection.getKeyRings();
+		System.out.println( "Read Pub KeyRing" );
+
+		// Add all recipient public keys to an array
+		PGPPublicKey[] recipientPublicKeys = new PGPPublicKey[pgpPublicKeyRingCollection.size()];
+		int i = 0;
+		while (keyRingIterator.hasNext()) {
+			PGPPublicKeyRing keyRing = keyRingIterator.next();
+			recipientPublicKeys[i++] = keyRing.getPublicKey();
+			System.out.println( "Iterate Pub Key" );
+		}
+		//FileOutputStream out = new FileOutputStream(outputFilePath);
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFilePath));
+		if(armor)
+		{
+			out = new ArmoredOutputStream(out);        
+		}
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+		PGPCompressedDataGenerator comData = new PGPCompressedDataGenerator(
+				PGPCompressedData.ZIP);
+
+		PGPUtil.writeFileToLiteralData(
+				comData.open(bOut),
+				PGPLiteralData.BINARY,
+				new File(inputFilePath));
+
+		comData.close();
+
+		PGPEncryptedDataGenerator cPk = new PGPEncryptedDataGenerator(
+				new JcePGPDataEncryptorBuilder(
+						PGPEncryptedData.CAST5)
+				.setWithIntegrityPacket(true)
+				.setSecureRandom(
+						new SecureRandom())
+				.setProvider("BC"));
+
+		for (PGPPublicKey publicKey : recipientPublicKeys) {
+			cPk.addMethod(
+					new JcePublicKeyKeyEncryptionMethodGenerator(publicKey)
+					.setProvider("BC"));
+		}
+
+		byte[] bytes = bOut.toByteArray();
+
+		OutputStream cOut = cPk.open(out, bytes.length);
+
+		cOut.write(bytes);
+
+		cOut.close();
+
+		out.close();
+		
+		System.out.println( "Encryption Process Completed" );
+	}
+	
+	public static ByteArrayInputStream encryptFile1(
+			String inputFilePath,
+			String outputFilePath,
+			String compositePublicKeyPath,
+			boolean armor
+			) throws Exception {
+		Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+
+		PGPPublicKeyRingCollection pgpPublicKeyRingCollection = new PGPPublicKeyRingCollection(
+				PGPUtil.getDecoderStream(new FileInputStream(compositePublicKeyPath)), new JcaKeyFingerprintCalculator());        
+
+		System.out.println( "Loaded Pub Key" );
+		Iterator<PGPPublicKeyRing> keyRingIterator = pgpPublicKeyRingCollection.getKeyRings();
+		System.out.println( "Read Pub KeyRing" );
+
+		// Add all recipient public keys to an array
+		PGPPublicKey[] recipientPublicKeys = new PGPPublicKey[pgpPublicKeyRingCollection.size()];
+		int i = 0;
+		while (keyRingIterator.hasNext()) {
+			PGPPublicKeyRing keyRing = keyRingIterator.next();
+			recipientPublicKeys[i++] = keyRing.getPublicKey();
+			System.out.println( "Iterate Pub Key" );
+		}
+		//FileOutputStream out = new FileOutputStream(outputFilePath);
+		
+		/*
+		OutputStream out = new BufferedOutputStream(new FileOutputStream(outputFilePath));
+		if(armor)
+		{
+			out = new ArmoredOutputStream(out);        
+		}
+		*/
+		ByteArrayOutputStream bOut = new ByteArrayOutputStream();
+
+		PGPCompressedDataGenerator comData = new PGPCompressedDataGenerator(
+				PGPCompressedData.ZIP);
+
+		PGPUtil.writeFileToLiteralData(
+				comData.open(bOut),
+				PGPLiteralData.BINARY,
+				new File(inputFilePath));
+
+		comData.close();
+
+		PGPEncryptedDataGenerator cPk = new PGPEncryptedDataGenerator(
+				new JcePGPDataEncryptorBuilder(
+						PGPEncryptedData.CAST5)
+				.setWithIntegrityPacket(true)
+				.setSecureRandom(
+						new SecureRandom())
+				.setProvider("BC"));
+
+		for (PGPPublicKey publicKey : recipientPublicKeys) {
+			cPk.addMethod(
+					new JcePublicKeyKeyEncryptionMethodGenerator(publicKey)
+					.setProvider("BC"));
+		}
+
+		byte[] bytes = bOut.toByteArray();
+
+		ByteArrayInputStream bytesOut = new ByteArrayInputStream(bytes);
+		//String strOut = new String(bytes);
+		//OutputStream cOut = cPk.open(out, bytes.length);
+
+		//cOut.write(bytes);
+
+		//cOut.close();
+
+		//out.close();
+		
+		System.out.println( "Encryption Process Completed" );
+		
+		return bytesOut;
+	}
+}
